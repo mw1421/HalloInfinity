a019c975fe63bdd1f639151be93d27b5bfcaae1c
mw1421 <mw1421@messiah.edu>
2021-11-28 19:28:02 -0500

    got the fricken API to pull some stuff

--- HalloInfinityReactProj/HalloInfinityReactProj/private/config.js
+++ HalloInfinityReactProj/HalloInfinityReactProj/private/config.js
@@ -1,4 +1,12 @@
 curl "https://cryptum.halodotapi.com/games/hmcc/motd"
     - H "Authorization: Cryptum-Token jJl5zGUUFK32mbkMY3MXdWsDTp9i2itKQBTds4N7UlG5N7bIGtXN3xQjOhIFCtXi"
         - H "Cryptum-API-Version: 2.3-alpha"
-            - H "Cryptum-Telemetry-Id: 48616C6f-0000-4e00-0000-415049000021"
+    - H "Cryptum-Telemetry-Id: 48616C6f-0000-4e00-0000-415049000021"
+
+fetch('https://cryptum.halodotapi.com/games/hmcc/motd', {
+    headers: {
+        Authorization: 'Cryptum-Token jJl5zGUUFK32mbkMY3MXdWsDTp9i2itKQBTds4N7UlG5N7bIGtXN3xQjOhIFCtXi',
+        'Cryptum-Api-Version': '2.3-alpha',
+        'Cryptum-Telemetry-Id': '48616C6f-0000-4e00-0000-415049000021'
+    }
+})
--- HalloInfinityReactProj/HalloInfinityReactProj/src/App.js
+++ HalloInfinityReactProj/HalloInfinityReactProj/src/App.js
@@ -4,31 +4,29 @@
 import './App.css';
 import React from 'react';
 
-var url = "https://cryptum.halodotapi.com/games/hmcc/motd";
-
-var xhr = new XMLHttpRequest();
-xhr.open("GET", url);
-
-xhr.setRequestHeader("Authorization", "Cryptum-Token jJl5zGUUFK32mbkMY3MXdWsDTp9i2itKQBTds4N7UlG5N7bIGtXN3xQjOhIFCtXi");
-xhr.setRequestHeader("Cryptum-API-Version", "2.3-alpha");
-xhr.setRequestHeader("Cryptum-Telemetry-Id", "48616C6f-0000-4e00-0000-415049000021");
-
-xhr.overrideMimeType('text/xml');
-
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-        console.log(xhr.status);
-        //console.log(xhr.responseText);
-        //var json = xhr.responseText.split(',');
-        //var title = json[0]
-        //console.log(title)
-    }
-};
-
-xhr.send();
 
 
 class App extends React.Component {
+
+    state = {
+        loading: true,
+        element: null,
+
+    }
+
+   async componentDidMount() {
+        const response = await fetch('https://cryptum.halodotapi.com/games/hmcc/motd', {
+            headers: {
+                Authorization: 'Cryptum-Token jJl5zGUUFK32mbkMY3MXdWsDTp9i2itKQBTds4N7UlG5N7bIGtXN3xQjOhIFCtXi',
+                'Cryptum-Api-Version': '2.3-alpha',
+                'Cryptum-Telemetry-Id': '48616C6f-0000-4e00-0000-415049000021'
+            }
+        })
+
+       const data = await response.json();
+       this.setState({ element: data.data[0], loading: false })
+       console.log(data)
+    }
 
     render() {
         return (
@@ -36,17 +34,24 @@ class App extends React.Component {
                 <div className='home'>
                     <div className='home-main'>
                         <h1>
-                            azh1n's React App
+                            Anatroc
                         </h1>
                     </div>
                     <div className='home-secondary'>
                         <h3>
-                            This is to showcase some stats from the popular game "Halo Infinite".
+                            Why did they make her ugly?
                         </h3>
                     </div>
                 </div>
-                <div>
-                    Title: 
+                <div className='home-MOTD-big'>
+                    {this.state.loading || !this.state.element ?
+                        <div> loading... </div> :
+                        <div className='home-MOTD-small'>
+                            <h2 className='MOTD-subtitle'> {this.state.element.subtitle} </h2>
+                            <h4 className='MOTD-title'> {this.state.element.title} </h4>
+                            <p> {this.state.element.message} </p>
+                            <img src={this.state.element.image_url} alt="MOTD" className='MOTD-image'/>
+                        </div>}
                 </div>
 
             </div>
